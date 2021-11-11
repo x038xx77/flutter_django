963eb101fc825f0703a6408cf8be1f8b02ab53f9
Pavel <62882598+x038xx77@users.noreply.github.com>
2021-11-11 16:57:29 +0300

    Delete dump/Django_server directory

--- dump/Django_server/sendemail/forms.py
+++
@@ -1,9 +0,0 @@
-# sendemail/forms.py
-from django import forms
-from django.utils.translation import gettext_lazy as _
-
-
-class ContactForm(forms.Form):
-    from_email = forms.EmailField(label=_('from_email'), required=True)
-    subject = forms.CharField(label=_('subject'), required=True)
-    message = forms.CharField(label=_('message'), widget=forms.Textarea(attrs={"cols":4, "rows":5}), required=True)
--- dump/Django_server/videoch_chat/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VideochChatConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'videoch_chat'
